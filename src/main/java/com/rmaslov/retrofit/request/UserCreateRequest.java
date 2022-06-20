d29ab8338a80c0f1ae8bd591ef7e7c3e5045fd39
Ilya <92110133+xiosamagh@users.noreply.github.com>
2022-06-20 19:56:02 +0300

    task2 - users request example

--- src/main/java/com/rmaslov/retrofit/request/UserCreateRequest.java
+++ src/main/java/com/rmaslov/retrofit/request/UserCreateRequest.java
@@ -1,5 +1,6 @@
-package com.rmaslov.retrofit.response;
+package com.rmaslov.retrofit.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,10 +8,12 @@ import lombok.ToString;
 @ToString
 @Getter
 @Setter
-public class UserResponse {
+@Builder
+public class UserCreateRequest {
     @ToString
     @Getter
     @Setter
+    @Builder
     public static class Address{
         private String street;
         private String suite;
@@ -18,9 +21,11 @@ public class UserResponse {
         private String zipcode;
         private Geo geo;
     }
+
     @ToString
     @Getter
     @Setter
+    @Builder
     public static class Company{
         private String name;
         private String catchPhrase;
@@ -29,12 +34,12 @@ public class UserResponse {
     @ToString
     @Getter
     @Setter
+    @Builder
     public static class Geo{
         private String lat;
         private String lng;
     }
 
-    private Integer id;
     private String name;
     private String username;
     private String email;
@@ -42,5 +47,6 @@ public class UserResponse {
     private String phone;
     private String website;
     private Company company;
+
 
 }
